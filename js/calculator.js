46f78bc8ad991b6d7c87b6c99ebadc48dd9f92ab
gabrielcicero45@gmail.com <gabrielcicero45@gmail.com>
2023-01-27 10:39:27 -0300

    fix: Addition symbol bugfix

--- js/calculator.js
+++ js/calculator.js
@@ -1,22 +1,25 @@
-const resultElement = document.querySelector('#result')
-const numbers = document.querySelectorAll('.number-button')
-const clearButton = document.querySelector('#AC')
-const additionButton = document.querySelector('#addition')
-const subtractionButton = document.querySelector('#subtraction')
-const equalsButton = document.querySelector('#equals')
+const resultElement = document.querySelector("#result");
+const numbers = document.querySelectorAll(".number-button");
+const clearButton = document.querySelector("#AC");
+const additionButton = document.querySelector("#addition");
+const subtractionButton = document.querySelector("#subtraction");
+const equalsButton = document.querySelector("#equals");
 
-clearButton.addEventListener('click', ()=>{
-  resultElement.textContent = ''
-})
-numbers.forEach(item =>{
-  item.addEventListener('click', ()=>{
-  resultElement.textContent = resultElement.textContent.concat(item.innerText)
-  })
-})
-additionButton.addEventListener('click', () => {
-  let previousValue = resultElement.textContent
-  resultElement.textContent = resultElement.textContent.concat('+')
-})
-equalsButton.addEventListener('click', () => {
-  resultElement.textContent = eval(resultElement.textContent)
-})
+clearButton.addEventListener("click", () => {
+  resultElement.textContent = "";
+});
+numbers.forEach((item) => {
+  item.addEventListener("click", () => {
+    resultElement.textContent = resultElement.textContent.concat(
+      item.innerText
+    );
+  });
+});
+additionButton.addEventListener("click", () => {
+  resultElement.textContent.charAt(resultElement.textContent.length - 1) == "+"
+    ? ""
+    : (resultElement.textContent = resultElement.textContent.concat("+"));
+});
+equalsButton.addEventListener("click", () => {
+  resultElement.textContent = eval(resultElement.textContent);
+});
